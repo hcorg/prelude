e19f5236f0faa60fb7a0b286db10ce311018ecb9
cierpuchaw <cierpuchaw@gmail.com>
2017-05-02 01:42:27 +0200

    Fixed dependency on solarized-theme when clean installing.

--- personal/personal-solarized.el
+++ personal/personal-solarized.el
@@ -1,4 +1,8 @@
 (prelude-require-packages '(solarized-theme
                             ))
 
+(disable-theme 'zenburn)
+(setq prelude-theme 'solarized-dark)
+(load-theme prelude-theme t)
+
 (custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
