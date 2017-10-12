687d236168acade407dfe990516fdbe9ea9bca1a
cierpuchaw <cierpuchaw@gmail.com>
2017-10-12 15:30:12 +0200

    Added helm-swoop.

---
+++ personal/personal-helm-swoop.el
@@ -0,0 +1,10 @@
+(prelude-require-packages '(helm-swoop
+                            ))
+
+(require 'helm)
+(require 'helm-swoop)
+
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
