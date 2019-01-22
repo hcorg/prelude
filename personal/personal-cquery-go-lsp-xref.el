86c73188ed879823dba99cf45d158cf34e608bf0
cierpuchaw <cierpuchaw@gmail.com>
2019-01-22 23:03:48 +0100

    Updated to handle recent lsp-mode changes.

--- personal/personal-cquery-go-lsp-xref.el
+++ personal/personal-cquery-go-lsp-xref.el
@@ -1,13 +1,11 @@
 (prelude-require-packages '(lsp-mode
                             lsp-ui
-                            lsp-go
                             company-lsp
                             helm-xref
                             cquery
                             ))
 
 (require 'cquery)
-(setq cquery-extra-init-params '(:index (:comments 0 :builtinTypes t) :cacheFormat "msgpack"))
 (cond
  ((string-equal system-type "gnu/linux")
   (setq cquery-extra-args '("--log-stdin-stdout-to-stderr" "--log-file=/tmp/cquery.log"))))
@@ -17,19 +15,17 @@
                                        xref-find-definitions-other-frame
                                        xref-find-references))
 
-(require 'lsp-imenu)
-(add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
-
+(require 'lsp-mode)
 (require 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+(require 'lsp-ui-imenu)
+
+(setq lsp-prefer-flymake nil)
+(setq lsp-ui-doc-enable nil)
+(setq lsp-ui-peek-enable nil)
+(setq lsp-ui-sideline-enable nil)
 
 (require 'helm-xref)
 (setq xref-show-xrefs-function 'helm-xref-show-xrefs)
 
-(require 'company-lsp)
-(push 'company-lsp company-backends)
-
-(add-hook 'c-mode-common-hook #'lsp-cquery-enable)
-
-(require 'lsp-go)
-(add-hook 'go-mode-hook #'lsp-go-enable)
+(add-hook 'c-mode-common-hook #'lsp)
+(add-hook 'go-mode-hook #'lsp)
