4e66887d626dc4e3213fc322d5ea5210d786e7f6
Konrad Grochowski <kgrochowski@n7space.com>
2020-11-24 14:02:31 +0100

    fixed personal/bash.el

--- personal/bash.el
+++ personal/bash.el
@@ -1,2 +1,7 @@
-(setq shell-file-name "bash")
-(setq shell-command-switch "-ic")
+; why????? this should work...: (setenv "BASH_ENV" (expand-file-name "~/.bashrc"))
+
+; https://stackoverflow.com/questions/10946219/emacs-compilation-mode-wont-see-bash-alias
+(defadvice compile (around use-bashrc activate)
+  "Load .bashrc in any calls to bash (e.g. so we can use aliases)"
+  (let ((shell-command-switch "-ic"))
+    ad-do-it))
