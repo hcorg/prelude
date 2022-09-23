36fe5c9cc0d48564c4d3dc162655e1f32b476efb
Konrad Grochowski <kgrochowski@n7space.com>
2022-09-23 15:37:38 +0200

    different asn1 mode added

--- personal/asn1.el
+++ personal/asn1.el
@@ -1,5 +1,9 @@
-(prelude-require-packages '(asn1-mode
-                            ))
+;;; ASN.1 Mode
 
-(add-to-list 'auto-mode-alist '("\\.asn\\'" . asn1-mode))
-(add-to-list 'auto-mode-alist '("\\.acn\\'" . asn1-mode))
+;(setq load-path (cons "~/emacs.d/personal/asn1-mode/" load-path))
+
+(load "~/.emacs.d/personal/asn1-mode/asn1-mode")
+
+(setq auto-mode-alist
+      (cons '("\\.[Aa][Ss][Nn]\\([1]\\|[pP][pP]?\\)?$" . asn1-mode)
+            auto-mode-alist))
