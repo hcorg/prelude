0201fc5aca8b179bf8667e43ce721ad955634e1f
Konrad Grochowski <kgrochowski@n7space.com>
2022-11-16 22:03:31 +0100

    multiple cursors auto config moved to repo

--- personal/personal-multiple-cursors.el
+++ personal/personal-multiple-cursors.el
@@ -3,5 +3,15 @@
 
 (require 'multiple-cursors)
 
+(setq mc/cmds-to-run-for-all
+      '(
+        c-electric-colon
+        c-electric-delete-forward
+        c-electric-semi&comma
+        c-electric-slash
+        c-electric-star
+        c-hungry-delete-forward
+        ))
+
 (global-set-key (kbd "C-c m") 'mc/mark-all-like-this-dwim)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
