c8d73fee000112bc8fc6c22693f74f6133199d4f
Wojciech Cierpucha <cierpuchaw@gmail.com>
2025-02-24 10:31:55 +0100

    Added lsp-sonarlint.

--- personal/personal-lsp.el
+++ personal/personal-lsp.el
@@ -1,11 +1,13 @@
 (prelude-require-packages '(lsp-mode
                             lsp-ui
                             lsp-treemacs
+                            lsp-sonarlint
                             helm-lsp))
 
 (require 'lsp-mode)
 (require 'lsp-ui)
 (require 'lsp-ui-imenu)
+(require 'lsp-sonarlint)
 
 (setq xref-prompt-for-identifier '(not xref-find-definitions
                                        xref-find-definitions-other-window
@@ -29,10 +31,13 @@
 (define-key lsp-ui-mode-map (kbd "C-c C-l g h") 'lsp-treemacs-call-hierarchy)
 
 (setq lsp-prefer-flymake nil)
+(setq lsp-ui-sideline-show-hover nil)
 ;; (setq lsp-ui-sideline-enable t)
 ;; (setq lsp-ui-doc-enable t)
 (setq lsp-ui-peek-enable t)
 (setq lsp-ui-peek-always-show t)
+
+(setq lsp-sonarlint-auto-download t)
 
 (add-hook 'c-mode-hook #'lsp)
 (add-hook 'c++-mode-hook #'lsp)
@@ -47,4 +52,5 @@
         "--completion-style=bundled"
         "--pch-storage=memory"
         "--header-insertion=never"
-        "--header-insertion-decorators=0"))
+        "--header-insertion-decorators=0")
+)
