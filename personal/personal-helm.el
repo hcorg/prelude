a0d0cd5a107bd0859afd3d7c65e0f0d395f00c4f
Wojciech Cierpucha <cierpuchaw@gmail.com>
2026-04-01 12:03:28 +0200

    Use relative path style in helm-grep.

--- personal/personal-helm.el
+++ personal/personal-helm.el
@@ -1,3 +1,5 @@
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-z")  'helm-select-action)
+
+(customize-set-variable 'helm-grep-file-path-style 'relative)
